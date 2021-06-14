11bf552bc36f01455e926f42f9c37a2866b0e78d
shreya1926 <74983980+shreya1926@users.noreply.github.com>
2021-06-15 00:02:46 +0530

    Add files via upload

---
+++ rate.js
@@ -0,0 +1,18 @@
+import React from 'react'
+export const rate ={} => {
+    return(
+        <div>
+            the top android apps as rated by the WTO 
+            1.Zomato
+            2.Uber
+            3.Urban
+            <h2>Reviews</h2>
+            <h1><p>Zomato</p></h1>
+            ratings:3 ***
+            <h1><p>Uber</p></h1>
+             ratings: 4 ****
+            <h1><p>Urban</p></h1> 
+            ratings: 5 *****           
+        </div>
+    )
+}
